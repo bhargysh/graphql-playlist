1ac84135c872c6dfcaed5f51544289d9e700d5d7
Bharg Sharma <bsha492@aucklanduni.ac.nz>
2021-12-01 15:37:35 +1100

    Add apollo so it can talk to graphql

--- README.md
+++ README.md
@@ -3,6 +3,20 @@ All course files for the GraphQL tutorial playlist on The Net Ninja YouTube chan
 
 Node modules are not included in the course files. After download, run npm install to install the modules (client and server folders)
 
-## Type relations
+# Running things
 
+## To run the graphql server
+
+```bash
+cd server
+nodemon app
+```
+
+## To run the react frontend
+
+```
+cd client
+nvm use 14.18.2
+yarn start
+```
 
--- client/src/App.js
+++ client/src/App.js
@@ -1,15 +1,20 @@
-import React, { Component } from 'react';
+import React, { Component } from "react";
+import BookList from "./components/BookList";
+import { ApolloClient, ApolloProvider } from "@apollo/client";
 
-// components
-import BookList from './components/BookList';
+const client = new ApolloClient({
+  uri: "http://localhost:4000/graphql",
+});
 
 class App extends Component {
   render() {
     return (
+      <ApolloProvider client={client}>
         <div id="main">
-          <h1>Ninja's Reading List</h1>
+          <h1>Bharg's Reading List</h1>
           <BookList />
-      </div>
+        </div>
+      </ApolloProvider>
     );
   }
 }
