6c53cd891b635f012c86ad758309810d490dc2c0
Bharg Sharma <bharg.sharma@rea-group.com>
2021-12-01 11:57:01 +1100

    Add root query for author and enable graphiql

--- server/schema/schema.js
+++ server/schema/schema.js
@@ -1,36 +1,66 @@
-const graphql = require('graphql');
-const { GraphQLObjectType, GraphQLString, GraphQLSchema } = graphql;
-const _ = require('lodash');
+const graphql = require("graphql");
+const {
+  GraphQLObjectType,
+  GraphQLString,
+  GraphQLSchema,
+  GraphQLID,
+  GraphQLInt,
+} = graphql;
+const _ = require("lodash");
 
 const dummyBookData = [
-    {name: 'Eye of the Sheep', genre: 'Drama', id: '1'},
-    {name: 'Intro to MLMs', genre: 'Non-Fiction', id: '2'},
-    {name: 'The Alchemist', genre: 'Fiction', id: '3'},
-    {name: 'Dune', genre: 'Sci-Fi', id: '4'},
-]
+  { name: "Eye of the Sheep", genre: "Drama", id: "1", authorId: "1" },
+  { name: "Intro to MLMs", genre: "Non-Fiction", id: "2", authorId: "3" },
+  { name: "The Alchemist", genre: "Fiction", id: "3", authorId: "4" },
+  { name: "Dune", genre: "Sci-Fi", id: "4", authorId: "2" },
+];
+
+const dummyAuthorData = [
+  { name: "Spongebob Sqaurepants", age: 32, id: "1" },
+  { name: "Alana Hawaii", age: 56, id: "2" },
+  { name: "Berg Shermer", age: 26, id: "3" },
+  { name: "Cool Guy", age: 49, id: "4" },
+];
 
 const BookType = new GraphQLObjectType({
-    name: 'Book',
-    fields: () => ({
-        id: { type: GraphQLString },
-        name: { type: GraphQLString },
-        genre: { type: GraphQLString },
-    })
-})
+  name: "Book",
+  fields: () => ({
+    id: { type: GraphQLID },
+    name: { type: GraphQLString },
+    genre: { type: GraphQLString },
+  }),
+});
+
+const AuthorType = new GraphQLObjectType({
+  name: "Author",
+  fields: () => ({
+    id: { type: GraphQLID },
+    name: { type: GraphQLString },
+    age: { type: GraphQLInt },
+  }),
+});
 
 const RootQuery = new GraphQLObjectType({
-    name: 'RootQueryType',
-    fields: { // don't need to wrap in a func as we don't care about order
-        book: {
-            type: BookType,
-            args: { id: { type: GraphQLString }}, // pass the id when making query for book
-            resolve(parent, args) { // how to get data from DB/other source
-                return _.find(dummyBookData, { id: args.id })
-            }
-        }
-    }
-})
+  name: "RootQueryType",
+  fields: {
+    // don't need to wrap in a func as we don't care about order
+    book: {
+      type: BookType,
+      args: { id: { type: GraphQLID } }, // pass the id when making query for book
+      resolve(parent, args) { // how to get data from DB/other source
+        return _.find(dummyBookData, { id: args.id });
+      },
+    },
+    author: {
+      type: AuthorType,
+      args: { id: { type: GraphQLID } },
+      resolve(parent, args) {
+        return _.find(dummyAuthorData, { id: args.id });
+      },
+    },
+  },
+});
 
 module.exports = new GraphQLSchema({
-    query: RootQuery
-})
+  query: RootQuery,
+});
