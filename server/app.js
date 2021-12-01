6cc934110bd19ea61bcebfa21f6e4c7c369236ad
Bharg Sharma <bharg.sharma@rea-group.com>
2021-12-01 11:37:22 +1100

    Add root query and pass schema to middleware

--- server/app.js
+++ server/app.js
@@ -1,9 +1,11 @@
 const express = require('express');
 const { graphqlHTTP } = require('express-graphql'); //allows express to understand graphql
+const schema = require('./schema/schema')
 
 const app = express();
 
 app.use('/graphql', graphqlHTTP({
+    schema
 }))
 
 app.listen(4000, () => {
--- server/schema/schema.js
+++ server/schema/schema.js
@@ -1,5 +1,13 @@
 const graphql = require('graphql');
-const { GraphQLObjectType, GraphQLString } = graphql;
+const { GraphQLObjectType, GraphQLString, GraphQLSchema } = graphql;
+const _ = require('lodash');
+
+const dummyBookData = [
+    {name: 'Eye of the Sheep', genre: 'Drama', id: '1'},
+    {name: 'Intro to MLMs', genre: 'Non-Fiction', id: '2'},
+    {name: 'The Alchemist', genre: 'Fiction', id: '3'},
+    {name: 'Dune', genre: 'Sci-Fi', id: '4'},
+]
 
 const BookType = new GraphQLObjectType({
     name: 'Book',
@@ -8,4 +16,21 @@ const BookType = new GraphQLObjectType({
         name: { type: GraphQLString },
         genre: { type: GraphQLString },
     })
+})
+
+const RootQuery = new GraphQLObjectType({
+    name: 'RootQueryType',
+    fields: { // don't need to wrap in a func as we don't care about order
+        book: {
+            type: BookType,
+            args: { id: { type: GraphQLString }}, // pass the id when making query for book
+            resolve(parent, args) { // how to get data from DB/other source
+                return _.find(dummyBookData, { id: args.id })
+            }
+        }
+    }
+})
+
+module.exports = new GraphQLSchema({
+    query: RootQuery
 })
