f51c1352ec86d0a5c278a51a8139ff010f71a20d
Bharg Sharma <bsha492@aucklanduni.ac.nz>
2021-12-01 15:53:29 +1100

    Enable cors so client can request to server

--- server/app.js
+++ server/app.js
@@ -2,13 +2,14 @@ const express = require('express');
 const { graphqlHTTP } = require('express-graphql'); //allows express to understand graphql
 const schema = require('./schema/schema')
 const mongoose = require("mongoose");
+const cors = require('cors');
 
 const app = express();
 mongoose.connect('mongodb+srv://bharg:db2810@graphql-tutorial.iye93.mongodb.net/myFirstDatabase?retryWrites=true&w=majority')
 mongoose.connection.once('open', () => {
     console.log('Connected to database');
 })
-
+app.use(cors());
 app.use('/graphql', graphqlHTTP({
     schema,
     graphiql: true
