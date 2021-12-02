64f1b2f1e754fbcc8ba710d21e1327ed8156a872
Bharg Sharma <bsha492@aucklanduni.ac.nz>
2021-12-02 17:05:49 +1100

    Add graphQL error handling

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,33 @@
 import React, { Component } from "react";
 import BookList from "./components/BookList";
 import AddBook from "./components/AddBook";
-import { ApolloClient, ApolloProvider, InMemoryCache } from "@apollo/client";
+import {
+  ApolloClient,
+  ApolloProvider,
+  InMemoryCache,
+  HttpLink,
+  from,
+} from "@apollo/client";
+import { onError } from "@apollo/client/link/error";
+
+const httpLink = new HttpLink({
+  uri: "http://localhost:4000/graphql",
+});
+
+const errorLink = onError(({ graphQLErrors, networkError }) => {
+  if (graphQLErrors)
+    graphQLErrors.forEach(({ message, locations, path }) =>
+      console.log(
+        `[GraphQL error]: Message: ${message}, Location: ${locations}, Path: ${path}`
+      )
+    );
+
+  if (networkError) console.log(`[Network error]: ${networkError}`);
+});
 
 const client = new ApolloClient({
-  uri: "http://localhost:4000/graphql",
-  cache: new InMemoryCache()
+  link: from([errorLink, httpLink]),
+  cache: new InMemoryCache(),
 });
 
 class App extends Component {
