bf4fa75e903f7bd0309c7d7d2761cef7b681c1dd
Bharg Sharma <bsha492@aucklanduni.ac.nz>
2021-12-02 17:04:40 +1100

    Reformat server app.js

--- server/app.js
+++ server/app.js
@@ -1,20 +1,26 @@
-const express = require('express');
-const { graphqlHTTP } = require('express-graphql'); //allows express to understand graphql
-const schema = require('./schema/schema')
+const express = require("express");
+const { graphqlHTTP } = require("express-graphql"); //allows express to understand graphql
+const schema = require("./schema/schema");
 const mongoose = require("mongoose");
-const cors = require('cors');
+const cors = require("cors");
 
 const app = express();
-mongoose.connect('mongodb+srv://bharg:db2810@graphql-tutorial.iye93.mongodb.net/myFirstDatabase?retryWrites=true&w=majority')
-mongoose.connection.once('open', () => {
-    console.log('Connected to database');
-})
+mongoose.connect(
+  "mongodb+srv://bharg:db2810@graphql-tutorial.iye93.mongodb.net/myFirstDatabase?retryWrites=true&w=majority"
+);
+mongoose.connection.once("open", () => {
+  console.log("Connected to database");
+});
+
 app.use(cors());
-app.use('/graphql', graphqlHTTP({
+app.use(
+  "/graphql",
+  graphqlHTTP({
     schema,
-    graphiql: true
-}))
+    graphiql: true,
+  })
+);
 
 app.listen(4000, () => {
-    console.log('Listening on port 4000')
+  console.log("Listening on port 4000");
 });
